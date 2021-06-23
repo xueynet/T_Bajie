d961865be29bc25f0a5e714e6acc3d15f374cf63
chenlian <chenlian@xuey.net>
2021-06-24 00:23:47 +0800

    底部样式调整实现

---
+++ wwwroot/m/js/footer.js
@@ -0,0 +1,13 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?341222a5aa508b29714bfb4370241cf3";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+$(function() {
+    $('#consultLink').on("click", function() {
+        console.log("start consult.");
+        $('#nb_icon_wrap').click();
+    });
+});
