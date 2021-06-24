c7c27cd1b7cc5e01f1fbb908eea2c08f2ca70c48
Swangxiaorong <Swangxiaorong@aliyun.com>
2021-06-24 15:33:06 +0800

    20210624

--- wwwroot/m/js/index.js
+++ wwwroot/m/js/index.js
@@ -14,4 +14,20 @@ $(function() {
         serviceswiper.slideTo($(this).index(), 250, false);
     })
     $('.wall ').jaliswall({ item: '.article ' });
+
+    // 首页轮播图
+    var swiper = new Swiper('.swiper-container-bn', {
+        direction: 'horizontal', // 垂直切换选项
+        loop: true, // 循环模式选项
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+        observer:true,
+        observeParents:true,
+      });
 });
+
+
+
+
