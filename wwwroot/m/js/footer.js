9138c7eace327f72c35573fcc435e629067695fd
chenlian <chenlian@xuey.net>
2021-06-25 00:35:26 +0800

    修正错误：服务详情页弹出层不能正常加载的问题

--- wwwroot/m/js/footer.js
+++ wwwroot/m/js/footer.js
@@ -1,4 +1,25 @@
 $(function() {
+    function publishRequire() {
+        layer.open({
+            type: 2,
+            area: ['100%', '100%'],
+            shadeClose: true,
+            offset: 't',
+            title: '发布需求',
+            content: '/m/channels/243.html'
+        });
+    }
+
+    function connectConsult() {
+        layer.open({
+            type: 2,
+            area: ['100%', '100%'],
+            shadeClose: true,
+            offset: 't',
+            title: '客服咨询',
+            content: 'http://p.qiao.baidu.com/cps/chat?siteId=16900753&userId=20703937&siteToken=341222a5aa508b29714bfb4370241cf3'
+        });
+    }
     $('.requireLink').on("click", function() {
         publishRequire();
     });
