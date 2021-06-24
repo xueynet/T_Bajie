8090bb843cb0cb7ad568ea939843839bd1ce9b0c
chenlian <chenlian@xuey.net>
2021-06-24 23:16:33 +0800

    修正问题：百度商桥加载缓慢

--- wwwroot/m/js/footer.js
+++ wwwroot/m/js/footer.js
@@ -1,16 +1,8 @@
-var _hmt = _hmt || [];
-(function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?341222a5aa508b29714bfb4370241cf3";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
 $(function() {
-    $('.consultLink').on("click", function() {
-        consultLink();
+    $('.requireLink').on("click", function() {
+        publishRequire();
     });
-    function consultLink(){
-        console.log("start consult.");
-        $('#nb_icon_wrap').click();
-    }
+    $('.consultLink').on("click", function() {
+        connectConsult();
+    });
 });
