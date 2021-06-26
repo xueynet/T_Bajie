a910b5e84be0b9b19146a233e88a9db5558690c5
chenlian <chenlian@xuey.net>
2021-06-26 18:03:27 +0800

    购买界面完善，流程未全部调通

--- wwwroot/m/js/footer.js
+++ wwwroot/m/js/footer.js
@@ -1,4 +1,9 @@
 $(function() {
+    //1.全局定义
+    var ACCESS_TOKEN_NAME = "xy_user_access_token";
+    var $token = localStorage.getItem(ACCESS_TOKEN_NAME);
+
+    //2.通用方法
     function publishRequire() {
         layer.open({
             type: 2,
@@ -20,10 +25,56 @@ $(function() {
             content: 'https://p.qiao.baidu.com/cps/chat?siteId=16900753&userId=20703937&siteToken=341222a5aa508b29714bfb4370241cf3'
         });
     }
+
+    function orderConfirm(siteId) {
+        console.log(siteId);
+        //2.3.1.检测登录状态
+        var params = {
+            "SiteId": siteId
+        };
+        $.ajax({
+            type: 'GET',
+            url: "/api/payment/GetPayType",
+            headers: { //请求头
+                Accept: "application/json; charset=utf-8",
+                Authorization: "Bearer " + $token,
+            },
+            data: JSON.stringify(params),
+            contentType: "application/json",
+            dataType: "json",
+            success: function(result) {
+                console.log(result);
+                if (!result.isLogin) {
+                    window.location.href = "/api/login/auth/Zbj?redirectUrl=" + encodeURIComponent(window.location.href);
+                    return;
+                }
+                //2.3.2.订单信息确认（手机号录入）界面呈现
+                var orderConfirmForm = $('#orderConfirmForm').html();
+
+                console.log(orderConfirmForm);
+                layer.open({
+                    type: 1,
+                    area: ['100%', '100%'],
+                    shadeClose: true,
+                    offset: 't',
+                    title: '确认订单信息',
+                    content: orderConfirmForm,
+                    cancel: function() {
+                        layer.msg('已取消订单', { time: 5000, icon: 6 });
+                    }
+                });
+            }
+        });
+    }
+
+    //3.事件绑定
     $('.requireLink').on("click", function() {
         publishRequire();
     });
     $('.consultLink').on("click", function() {
         connectConsult();
+    });
+    $('.orderLink').on("click", function() {
+        orderConfirm($(this).attr("data-siteId"));
     });
 });
