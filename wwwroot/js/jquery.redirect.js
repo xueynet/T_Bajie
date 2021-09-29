ee4167088faa4ea2251c95352a6406ff9d4920cd
Mrchenscx <474421987@qq.com>
2021-09-29 13:16:20 +0800

    自动跳转gc更改

--- wwwroot/js/jquery.redirect.js
+++ wwwroot/js/jquery.redirect.js
@@ -4,7 +4,7 @@
 jQuery(document).ready(function($) {
     let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i);
     if (flag) {
-        var redirectUrl = "https://gc.zbj.com/m/";
+        var redirectUrl = "/m/";
         location = redirectUrl;
     }
 });
