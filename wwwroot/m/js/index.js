81bf5316945155b32101fb2dea1dcb5edf34e83e
Mrchenscx <474421987@qq.com>
2021-11-26 14:06:06 +0800

    增加新专题，移动端首页banner自动高度添加

--- wwwroot/m/js/index.js
+++ wwwroot/m/js/index.js
@@ -18,6 +18,7 @@ $(function() {
     var bannetSwiper = new Swiper('.swiper-container-bn', {
         direction: 'horizontal', // 垂直切换选项
         loop: true, // 循环模式选项
+        autoHeight: true,
         autoplay: {
             delay: 3000,
             disableOnInteraction: false,
